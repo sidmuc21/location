3e55c5ee05cec03cc1dc66db68d8dc0387a814cd
SidritMucaj <sidmuc@htl-shkoder.com>
2025-09-15 10:54:43 +0200

    fcommit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+	let latitude = null;
+	let longitude = null;
+	let accuracy = null;
+	let errorMessage = null;
+ 
+	function getLocation() {
+		navigator.geolocation.getCurrentPosition(showPosition, showError);
+	}
+ 
+	function showPosition(position) {
+		latitude = position.coords.latitude;
+		longitude = position.coords.longitude;
+		accuracy = position.coords.accuracy;
+		errorMessage = null;
+	}
+ 
+	function showError(error) {
+		errorMessage = "Es gibt ein Fehler.";
+	}
+</script>
+ 
+<div class=" h-screen flex justify-center p-50 bg-linear-to-b from-[#000814] to-[#0c0a3b]">
+  <div class="w-full p-6">
+    
+    <h1 class="text-2xl font-semibold font-display text-white mb-4 text-center"> GEOLOCATION</h1>
+    <div class="flex justify-center mb-4">
+      <button 
+        on:click={getLocation} 
+        class="p-3 hover:text-blue-400 text-white font-medium rounded"
+      >
+        STANDORT ABRUFEN
+      </button>
+    </div>
+
+    {#if errorMessage}
+      <p class="text-red-600 text-sm mb-4 text-center">{errorMessage}</p>
+    {/if}
+
+    <ul class="text-blue-400 ">
+      <li class="my-2 font-bold">BREITENGRAD: {latitude}</li>
+      <li class="my-2 font-bold">LANGENGRAD: {longitude}</li>
+      <li class="my-2 font-bold">GENAUIGKEIT (m): {accuracy}</li>
+    </ul>
+  </div>
+</div>
